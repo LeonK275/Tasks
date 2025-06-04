5878497e7e5a9acc44cb690291d38f0a5573f476
Leon <mail@leonkuehn.de>
2025-06-04 17:22:18 +0200

    new retrofit code

--- TaskApp/app/src/main/java/com/leon/taskapp/MainActivity.java
+++ TaskApp/app/src/main/java/com/leon/taskapp/MainActivity.java
@@ -8,6 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.leon.taskapp.model.Task;
+import com.leon.taskapp.network.ApiClient;
+import com.leon.taskapp.network.TaskApiService;
+
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +32,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TaskApiService taskApiService = ApiClient.getTaskService();
+        taskApiService.getTasks().enqueue(new retrofit2.Callback<List<Task>>() {
+            @Override
+            public void onResponse(Call<List<Task>> call, Response<List<Task>> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Task>> call, Throwable t) {
+
+            }
+        });
+
+        Log.d;
     }
 }
